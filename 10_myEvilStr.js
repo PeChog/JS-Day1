cffc72642d5b1bfe9d6a66e3135b80bca26a013f
Pierre-Etienne <pierre-etienne@MacBook-Pro-de-Pierre-Etienne.local>
2023-01-02 16:37:10 +0100

    second commit

--- 10_myEvilStr.js
+++ 10_myEvilStr.js
@@ -1,6 +1,6 @@
 const evilStr = (str) => {
   let newStr = "";
-  for (let i = str.length; i >= 0; i--) {
+  for (let i = str.length - 1; i >= 0; i--) {
     newStr = newStr + str[i];
   }
   return newStr;
